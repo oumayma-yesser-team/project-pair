4252f7f295497ac12819297904679140970364ec
mohamed-yesser <yesser305@gmail.com>
2024-03-12 10:02:21 +0100

    stepdone

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+$('.books img').click(function() {
+    var imgSrc = $(this).attr('src');
+    var imgid = $(this).attr('id');
+    switchToBookDetails(imgSrc); 
+});
+
+function switchToBookDetails(source) {
+   $("body").load("main2.html", function() {
+       $('#imgDes').attr('src', source);
+       
+   });
+}
+
+var home0 = document.getElementById('homeBtn')
+
+
+home0.addEventListener('click', function() {
+    backHome();
+});
+
+function backHome() {
+    $("body").load("main3.html");
+}
