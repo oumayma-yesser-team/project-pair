8c83282d5bd0e8e0cb8af53d86312ad3c5eb67cb
mohamed-yesser <yesser305@gmail.com>
2024-03-12 15:02:57 +0100

    v2

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function makebook(title, author, price) {
 
 var annakarenina=makebook("Anna Karenina", "Leo Toltsoy",50)
 var wutheringheights=makebook("Wuthering Heights","Emily Brontë",40)
-var thebrotherskaramasov=makebook("The Brothers Karamasov","Fyodor Dostoevsky",35)
+var thebrotherskaramasov=makebook("The Brothers Karamazov" , "Fyodor Dostoevsky",35)
 var janeEyre=makebook("Jane Eyre","Charlotte Brontë",40)
 var crimeAndPunishement=makebook("Crime And Punishment", "Fyodor Dostoevsky", 45)
 var thefabricofthecosmos=makebook("The Fabric Of The Cosmos", "Brian Greene", 35)
 var thesecrethistory=makebook("The Secret History","Donna Tartt",60)
-var thegoldfinch=makebook("The Golfinch","Donna Tartt",50)
+var thegoldfinch=makebook("The Goldfinch","Donna Tartt",50)
 var warandpeace=makebook("War And Peace", "Leo Toltsoy",70)
 var thePictureOfDorianGray=makebook("The Picture Of Dorian Gray","Oscar Wilde",30)
 var metamporphosis=makebook("The Metamorphosis","Franz Kafka",30)
@@ -59,17 +59,46 @@ makelist(theFaultInour)
 makelist(harryPotter)
 makelist(thehungergames)
 
+
+
 $('.books img').click(function() {
     var imgSrc = $(this).attr('src');
     var imgid = $(this).attr('id');
-    switchToBookDetails(imgSrc); 
+    switchToBookDetails(imgSrc,imgid); 
 });
+  
+function switchToBookDetails(source,imgidof) {
+    var counter=0 
 
-function switchToBookDetails(source) {
    $("body").load("main2.html", function() {
        $('#imgDes').attr('src', source);
 
+
+       for ( var z = 0 ; z < list.length ; z++ ){
+        console.log(list[z].price ," the list")   
+        if (list[z].title.toLowerCase() === imgidof.toLowerCase())
+
+       {$("#DescriptionP").html(list[z].title)
+        var oneprice =list[z].price 
+        test(oneprice)
+    }
+
+       
+       function test (x){    
+        $('.addToCartButton').click(function() {
+            
+            console.log(counter)
+          return    counter=counter+(x)
+                
+        });
+
+        return counter
+    }
+    
+    }
+    
    });
+
 }
 
 var home0 = document.getElementById('homeBtn')
@@ -83,10 +112,25 @@ function backHome() {
     $("body").load("main3.html");
 }
 
+
+var divs= $(".books")
+// console.log(divs[0].outerText , 'outer ht is')
+var elem = $('#book1')
+
+
+
 $("#btnOk").click(function(){
     for(var i=0; i<list.length; i++){
         if(($("#reasearchBar").val()).toLowerCase()===list[i].title.toLowerCase()){
-            
+            var titre=$("#reasearchBar").val()
+            for(var j=0; j<divs.length; j++) {
+                var imgId=divs[j].outerText
+                console.log('imgId is' , imgId )
+                if(imgId!==titre) {
+                    $(divs[j]).hide()
+
+                }
+            }
         }
     }
 })
