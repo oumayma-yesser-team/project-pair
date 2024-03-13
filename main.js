c3f8ff2e1043a85471aa434f648a77da6a6d7cab
mohamed-yesser <yesser305@gmail.com>
2024-03-13 04:16:30 +0100

    Vf

--- main.js
+++ main.js
@@ -1,1 +1,209 @@
 
+
+function makebook(title, author, price,description) {
+    var obj={}
+    obj.title=title
+    obj.author=author
+    obj.price=price
+    obj.description=description
+    return obj
+}
+
+var annakarenina=makebook("Anna Karenina", "Leo Toltsoy",50,"Acclaimed by many as the world's greatest novel, Anna Karenina provides a vast panorama of contemporary life in Russia and of humanity in general.")
+var wutheringheights=makebook("Wuthering Heights","Emily Brontë",40,"At the centre of this novel is the passionate love between Catherine Earnshaw and Heathcliff - recounted with such emotional intensity that a plain tale of the Yorkshire moors acquires the depth and simplicity of ancient tragedy.This best-selling Norton Critical Edition is based on the 1847 first edition of the novel. For the Fourth Edition, the editor has collated the 1847 text with several modern editions")
+var thebrotherskaramasov=makebook("The Brothers Karamazov" , "Fyodor Dostoevsky",35,"The Brothers Karamazov is a murder mystery, a courtroom drama, and an exploration of erotic rivalry in a series of triangular love affairs involving the “wicked and sentimental” Fyodor Pavlovich Karamazov")
+var janeEyre=makebook("Jane Eyre","Charlotte Brontë",40,"A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Brontë's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics")
+var crimeAndPunishement=makebook("Crime And Punishment", "Fyodor Dostoevsky", 45,"Raskolnikov, a destitute and desperate former student, wanders through the slums of St Petersburg and commits a random murder without remorse or regret. He imagines himself to be a great man")
+var thefabricofthecosmos=makebook("The Fabric of the Cosmos", "Brian Greene", 35,"From Brian Greene, one of the world's leading physicists, comes a grand tour of the universe that makes us look at reality")
+var thesecrethistory=makebook("The Secret History","Donna Tartt",60,"Under the influence of their charismatic classics professor, a group of clever, eccentric misfits at an elite New England college discover a way of thinking and living that is a world away")
+var thegoldfinch=makebook("The Goldfinch","Donna Tartt",50,"Aged thirteen, Theo Decker, son of a devoted mother and a reckless, largely absent father, survives an accident that otherwise tears his life apart. Alone and rudderless in New York, he is taken in by the family of a wealthy friend. He is tormented by an unbearable longing for his mother")
+var warandpeace=makebook("War And Peace", "Leo Toltsoy",70,"In Russia's struggle with Napoleon, Tolstoy saw a tragedy that involved all mankind. War and Peace broadly focuses on Napoleon invasion of Russia in 1812 and follows three of the most well-known characters in literature: Pierre Bezukhov")
+var thePictureOfDorianGray=makebook("The Picture Of Dorian Gray","Oscar Wilde",30,"Oscar Wildes only novel is the dreamlike story of a young man who sells his soul for eternal youth and beauty. In this celebrated work Wilde forged a devastating portrait of the effects of evil and debauchery on a young aesthete in late-19th-century England. Combining elements of the Gothic horror novel and decadent French fiction")
+var metamporphosis=makebook("The Metamorphosis","Franz Kafka",30,"As Gregor Samsa awoke one morning from uneasy dreams he found himself transformed in his bed into a gigantic insect. He was laying on his hard, as it were armor-plated, back and when he lifted his head" )
+var thestranger=makebook("The Stranger","Albert Camus",20,"Published in 1942 by French author Albert Camus, The Stranger has long been considered a classic of twentieth-century literature.")
+var thecatcherintherye=makebook("The Catcher In The Rye", "J.D. Salinger", 25,"In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line")
+var notesfromunderground=makebook("Notes From The Underground","Fyodor Dostoevsky",30,"Dostoevskys most revolutionary novel, Notes from Underground marks the dividing line between nineteenth- and twentieth-century fiction, and between the visions")
+var theBellJar=makebook("The Bell Jar","Sylvia Plath",40,"The Bell Jar chronicles the crack-up of Esther Greenwood: brilliant, beautiful, enormously talented, and successful, but slowly going under—maybe for the last time. Sylvia Plath masterfully draws the reader into Esther's breakdown")
+var georgeorwell=makebook("1984", "George Orwell",35,"A masterpiece of rebellion and imprisonment where war is peace freedom is slavery and Big Brother is watching Thought Police Big Brother Orwellian These words have entered our vocabulary because of George Orwell")
+var thegrandgatsby=makebook("The Great Gatsby","F. Scott Fitzgerald",40,"The Great Gatsby, F. Scott Fitzgerald's third book, stands as the supreme achievement of his career. This exemplary novel of the Jazz Age has been acclaimed by generations of readers.")
+var tokillamocking=makebook("To Kill a Mocking Bird","Harper Lee", 25,"Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred")
+var theFaultInour=makebook("The Fault In Our Stars","John Green",35, "Despite the tumor-shrinking medical miracle that has bought her a few years, Hazel has never been anything but terminal, her final chapter inscribed upon diagnosis")
+var harryPotter=makebook("Harry Potter and the Sorcerer's Stone", "J.K. Rowling",25,"Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal")
+var thehungergames=makebook("The Hunger Games","Suzanne Collins",45,"In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line")
+var OfMiceandMen=makebook("Of Mice and Men", "john steinbeck",50,"Compassionate, dramatic, and deeply moving, To Kill A Mockingbird takes readers to the roots of human behavior - to innocence and experience, kindness and cruelty, love and hatred")
+
+var list=[]
+function makelist(object) {
+    list.push(object)
+}
+
+makelist(annakarenina)
+makelist(wutheringheights)
+makelist(thebrotherskaramasov)
+makelist(janeEyre)
+makelist(crimeAndPunishement)
+makelist(thefabricofthecosmos)
+makelist(thesecrethistory)
+makelist(thegoldfinch)
+makelist(warandpeace)
+makelist(thePictureOfDorianGray)
+makelist(metamporphosis)
+makelist(thestranger)
+makelist(thecatcherintherye)
+makelist(notesfromunderground)
+makelist(theBellJar)
+makelist(georgeorwell)
+makelist(thegrandgatsby)
+makelist(tokillamocking)
+makelist(theFaultInour)
+makelist(harryPotter)
+makelist(thehungergames)
+makelist(OfMiceandMen)
+
+
+$('.books img').click(function() {
+    var imgSrc = $(this).attr('src');
+    var imgid = $(this).attr('id');
+    switchToBookDetails(imgSrc,imgid); 
+});
+var counter = 0  
+
+function switchToBookDetails(source,imgidof) {
+    
+
+   $("body").load("main2.html", function() {
+       $('#imgDes').attr('src', source);
+
+
+       for ( var z = 0 ; z < list.length ; z++ ){
+         
+        if (list[z].title.toLowerCase() === imgidof.toLowerCase())
+
+       {$("#DescriptionP").html(list[z].description)
+       $(".book-title").html(list[z].title)
+        var pricee =list[z].price 
+        cartt(pricee)
+    }
+
+       
+       function cartt (x){    
+        $('.addToCartButton').click(function() {
+            
+            console.log(counter)
+          return    counter=counter+(x)
+                
+        });
+
+        return counter
+    }
+    
+    }
+    
+   });
+
+}
+
+
+
+
+//     $(".addToCartButton").click(function(){
+//         var imgId = $(this).closest('.books').children('img').attr('id');
+        
+    
+//         for ( var z = 0 ; z < list.length ; z++ ){
+         
+//             if (list[z].title.toLowerCase() === imgId.toLowerCase())
+    
+//            {
+          
+//             var oneprice =list[z].price 
+//             test(parseInt(oneprice))
+//         }
+    
+    
+    
+    
+    
+    
+//     }});
+// ;
+
+
+
+
+
+
+// function test (x){    
+    
+        
+//         console.log(counter)
+//       return    counter=counter+(x)
+            
+//  };
+
+//  $("btnCart").html(test())
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+var home0 = document.getElementById('homeBtn')
+
+
+home0.addEventListener('click', function() {
+    backHome();
+});
+
+function backHome() {
+    $("body").load("main3.html");
+}
+
+
+var divs= $(".books")
+// console.log(divs[0].outerText , 'outer ht is')
+var elem = $('#book1')
+
+
+
+$("#btnOk").click(function(){
+    var found = false;
+    var searchTitle = $("#reasearchBar").val().toLowerCase();
+    for(var i=0; i<list.length; i++){
+        if(searchTitle === list[i].title.toLowerCase()){
+            found = true;
+            var titre = $("#reasearchBar").val();
+            for(var j=0; j<divs.length; j++) {
+                var imgId = divs[j].outerText;
+                
+                if(imgId !== titre) {
+                    $(divs[j]).hide();
+                }
+            }
+        }
+    }
+    if (!found) {
+        alert("Book not found");
+    }
+});
+
+
+
+var cart = document.getElementById('btnCart')
+
+
+cart.addEventListener('click', function() {
+    CartSection();
+});
+
+function CartSection() {
+    $("body").load("main4.html");
+}
